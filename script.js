fef36f37050c8a8375c2ca41dd36e60b5af59c7b
Glitch (glitch-hello-website) <none>
2021-05-20 23:25:29 +0000

    🎭🍀 Checkpoint ./style.css:23568145/208 ./script.js:23568145/360 ./index.html:23568145/779

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints a message in the browser's dev tools console
-console.log("Hello 🌎");
+$().html();
