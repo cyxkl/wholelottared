ec12843e70b10deab9d89c8778fa4cdf03eb2f7f
Glitch (glitch-hello-website) <none>
2021-05-20 23:37:18 +0000

    🐴🌸 Checkpoint ./script.js:23568145/526 ./index.html:23568145/83:23568162/1523 ./style.css:23568162/790

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-$().html();
+$(".searchButton").click(function() {
+  let api_url = "https://api.soundcloud.com/tracks?q=test&client_id=5aa8e389ba4e24b6106af5159ab3e344"
+  fetch(api_url)
+  
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(songData) {
+    console.log(songData);
+  });
+});
