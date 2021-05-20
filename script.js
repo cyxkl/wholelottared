1935fc42cd3c9f888df3680a4934f2c6e982aa5d
Glitch (glitch-hello-website) <none>
2021-05-20 23:49:27 +0000

    🎮🖇 Checkpoint ./index.html:23568145/656:23568162/2760 ./script.js:23568145/548 ./style.css:23568162/461

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-$(".searchButton").click(function() {
-  let api_url = "https://api.soundcloud.com/tracks?q=test&client_id=5aa8e389ba4e24b6106af5159ab3e344"
-  fetch(api_url)
-  
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(songData) {
-    console.log(songData);
-  });
+$(".search-button").click(function() {
+  let input = $(".search-term").val();
+  let url = "https://api.soundcloud.com/tracks?q=" + input +"&client_id=5aa8e389ba4e24b6106af5159ab3e344"
+  console.log(input);
+
+      fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(songData) {
+            console.log(songData);
+        });
 });
