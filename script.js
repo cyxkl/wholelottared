a5879c892db728b677b1d876c05a8bbb936a6e39
Glitch (glitch-hello-website) <none>
2021-05-21 00:01:17 +0000

    💃🏐 Checkpoint ./index.html:23568162/2803 ./script.js:23568145/430 ./style.css:23568162/1433

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+<script src="https://connect.soundcloud.com/sdk/sdk-3.3.2.js"></script>
+<script>
+SC.initialize({
+  client_id: 'YOUR_CLIENT_ID'
+});
+
+var track_url = 'https://soundcloud.com/forss/flickermood';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+});
+</script>
+
 $(".search-button").click(function() {
   let input = $(".search-term").val();
   let url = "https://api.soundcloud.com/tracks?q=" + input +"&client_id=5aa8e389ba4e24b6106af5159ab3e344"
@@ -10,4 +22,10 @@ $(".search-button").click(function() {
         .then(function(songData) {
             console.log(songData);
         });
+});
+
+$(".search-term").keyup(function(event) {
+  if(event.keyCode === 13) {
+    $(".search-button").click();
+  }
 });
